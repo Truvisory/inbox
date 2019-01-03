85705cdb47221721602cb125536aaac38dd68746
TONY ADAMS <truvisory@TONYs-MacBook-Pro.local>
2019-01-02 21:44:40 -0700

    Api added and a few user stories met

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Toolbar from './components/Toolbar'
+import Message from './components/Message'
+import MessageList from './components/MessageList'
+const url = 'http://localhost:8082/api/messages'
 
 class App extends Component {
+  constructor(){
+    super()
+      this.state = {
+        messages: []
+      }
+  }
+
+  async componentDidMount() {
+    const response = await fetch(url)
+    const json = await response.json()
+    this.setState({messages: json})
+  }
+
+  starClick = (e) => {
+    // console.log(e.target.value)
+    let filteredProducts = 
+      this.state.messages.filter((message) => message.id === e.target.id * 1 )
+      console.log(e.target[2])
+    this.setState({starred: !this.state.starred})
+  }
+
   render() {
+    console.log(this.state.messages)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <Toolbar />
+        {/* <Message /> */}
+        {this.state.messages[0]
+          ? <MessageList 
+              messages={this.state.messages}
+              starClick={this.starClick} />
+          : <div></div>}
       </div>
     );
   }
