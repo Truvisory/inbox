539b9bd1a97cf73078cf603c952c3c64d339e581
TONY ADAMS <truvisory@TONYs-MacBook-Pro.local>
2019-01-04 21:21:40 -0700

    Button state changes when no messages selected

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Toolbar = (props) => {
+  const disabledWhenNothingSelected = props.selectedIndicator === 0 ? "disabled" : ""
     return ( 
       <div className="row toolbar">
         <div className="col-md-12">
@@ -19,30 +20,45 @@ const Toolbar = (props) => {
                 ? "fa fa-check-square-o"
                 : props.selectedIndicator === 0
                   ? "fa fa-square-o" 
-                  : "fa fa-minus-square-o" }>
+                  : "fa fa-minus-square-o"}>
             </i>
           </button>
 
-          <button className="btn btn-default" onClick={props.markedRead}>Mark As Read</button>
+          <button 
+            className="btn btn-default"
+            disabled={disabledWhenNothingSelected} 
+            onClick={props.markedRead}>Mark As Read</button>
 
-          <button className="btn btn-default" onClick={props.markedUnread}>Mark As Unread</button>
+          <button 
+            className="btn btn-default"
+            disabled={disabledWhenNothingSelected}
+            onClick={props.markedUnread}>Mark As Unread</button>
 
-          <select className="form-control label-select" onChange={props.applyLabel}>
-            <option>Apply label</option>
-            <option value="dev">dev</option>
-            <option value="personal">personal</option>
-            <option value="gschool">gschool</option>
+          <select 
+            className="form-control label-select" 
+            disabled={disabledWhenNothingSelected} 
+            onChange={props.applyLabel}>
+              <option>Apply label</option>
+              <option value="dev">dev</option>
+              <option value="personal">personal</option>
+              <option value="gschool">gschool</option>
           </select>
 
-          <select className="form-control label-select" onChange={props.removeLabel}>
-            <option>Remove label</option>
-            <option value="dev">dev</option>
-            <option value="personal">personal</option>
-            <option value="gschool">gschool</option>
+          <select 
+            className="form-control label-select" 
+            disabled={disabledWhenNothingSelected} 
+            onChange={props.removeLabel}>
+              <option>Remove label</option>
+              <option value="dev">dev</option>
+              <option value="personal">personal</option>
+              <option value="gschool">gschool</option>
           </select>
 
-          <button className="btn btn-default" onClick={props.delete}>
-            <i className="fa fa-trash-o"></i>
+          <button 
+            className="btn btn-default" 
+            disabled={disabledWhenNothingSelected} 
+            onClick={props.delete}>
+              <i className="fa fa-trash-o"></i>
           </button>
         </div>
       </div>
